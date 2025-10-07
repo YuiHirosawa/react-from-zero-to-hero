8179fa7b8159c8a05e5808097ad7d56eb2ba0778
YuiHirosawa <1173948138@qq.com>
2025-10-07 19:13:15 +1100

    day two style and loop

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,65 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+export default function App() { // function
+  // 1. Javascript
+  const [count, setCount] = useState(0);
+  const title = 'React: From Zero to Hero';
+  // part 9. Loops
+  // const fruits = [
+  //   {
+  //     title: 'Apple',
+  //     count: 12,
+  //     options: [
+  //       {
+  //         title: 'Golden',
+  //         count: 8,
+  //       }
+  //     ]
+  //   },
+  //   {
+  //     title: 'Banana',
+  //     count: 8,
+  //     options: [
+  //       {
+  //         title: 'Angola',
+  //         count: 8,
+  //       }
+  //     ]
+  //   }
+  // ]
+
+  // 2. React hooks
+
+  // 3. JSX
   return (
     <>
+    {/* part 9 related */}
+      {/* <ul>
+        {fruits.map((fruit, index) =>
+          <li key={index}>
+            <span>{fruit.title}, </span>
+            <span>Quantity: {fruit.count}</span>
+            <ul>
+              {fruit.options.map((option, index) =>
+              <li key={index}>
+                <span>{option.title}, </span>
+                <span>Quantity: {option.count}</span>
+              </li>)}
+            </ul>  
+          </li>
+        )}
+      </ul> */} 
+    {/* part 10. style related */}
+    <header style={{
+      backgroundColor: 'green'
+    }}>
+      <h1>Hello Yui</h1>
+      <p>You are a good person</p>
+
+    </header>
+
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -16,14 +70,11 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>React: From Zero to Hero</h1>
+      <h1>{title}</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
@@ -32,4 +83,4 @@ function App() {
   )
 }
 
-export default App
+// export default App
